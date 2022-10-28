5f5c8337e78e18be03ad9b13215ef6ec00c5fc25
Chibuokem Egbuchulam <talk2chibu@gmail.com>
2022-10-28 23:48:16 +0100

    fix: change website name

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Container, Section } from "../../components/reuseable";
-import { ProfileLinks } from "./styles";
+import { ProfileContainer, ProfileLinks } from "./styles";
 
 const Profile = () => {
   return (
     <Section>
       <Container>
-        <ul>
-          <li>
-            <ProfileLinks to="/users">Users</ProfileLinks>
-          </li>
-          <li>
-            <ProfileLinks to="/about">About</ProfileLinks>
-          </li>
-        </ul>
+        <ProfileContainer>
+          <h2>
+            Web page undergoing Construction, Please check back in an hour
+          </h2>
+          <ul>
+            <li>
+              <ProfileLinks to="/users">Users</ProfileLinks>
+            </li>
+            <li>
+              <ProfileLinks to="/about">About</ProfileLinks>
+            </li>
+          </ul>
+        </ProfileContainer>
       </Container>
       <Outlet />
     </Section>
--- src/pages/Profile/styles.jsx
+++ src/pages/Profile/styles.jsx
@@ -1,7 +1,14 @@
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
-export const ProfileContainer = styled.div``;
+export const ProfileContainer = styled.div`
+  color: ${({ theme }) => theme.color.black};
+
+  h2 {
+    color: ${({ theme }) => theme.color.black};
+  }
+`;
 export const ProfileLinks = styled(NavLink)`
   color: #000;
+  font-weight: 600;
 `;
