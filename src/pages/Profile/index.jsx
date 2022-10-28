c75f289322cb40813b77e8049ccc88ee7ec728f0
Chibuokem Egbuchulam <talk2chibu@gmail.com>
2022-10-28 23:27:52 +0100

    build: create profile page and style

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Container, Section } from "../../components/reuseable";
+import { ProfileLinks } from "./styles";
 
 const Profile = () => {
   return (
     <Section>
-      <Container>Profile</Container>
+      <Container>
+        <ul>
+          <li>
+            <ProfileLinks to="/users">Users</ProfileLinks>
+          </li>
+          <li>
+            <ProfileLinks to="/about">About</ProfileLinks>
+          </li>
+        </ul>
+      </Container>
       <Outlet />
     </Section>
   );
--- src/pages/Profile/styles.jsx
+++ src/pages/Profile/styles.jsx
@@ -1,1 +1,7 @@
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+
+export const ProfileContainer = styled.div``;
+export const ProfileLinks = styled(NavLink)`
+  color: #000;
+`;
