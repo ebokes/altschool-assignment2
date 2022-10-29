18f6a8655891cb4ca49a7cf39f938dd7f9516bd4
Chibuokem Egbuchulam <talk2chibu@gmail.com>
2022-10-29 02:28:17 +0100

    style: style Users page

--- src/pages/Users/index.jsx
+++ src/pages/Users/index.jsx
@@ -1,7 +1,129 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+// import { NavLink, useParams } from "react-router-dom";
+import axios from "axios";
+import Loading from "../../components/Loading";
+import {
+  ButtonGroup,
+  Card,
+  CardWrapper,
+  Paginate,
+  UsersContainer,
+  UsersWrapper,
+} from "./styles";
 
-const index = () => {
-  return <div>index</div>;
+const Users = () => {
+  const [users, setUsers] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  // const [page, setPage] = useState(1);
+
+  const client = axios.create({
+    baseURL: "https://randomuser.me/",
+  });
+
+  useEffect(() => {
+    async function getUsers() {
+      try {
+        setLoading(true);
+        const response = await client.get("/api/?results=60");
+        setUsers(response.data.results);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getUsers();
+  }, []);
+
+  console.log(users);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  const PER_PAGE = 12;
+  const total = users?.length;
+  const pages = Math.ceil(total / PER_PAGE);
+  const skip = page * PER_PAGE - PER_PAGE;
+
+  return (
+    <UsersWrapper>
+      <UsersContainer>
+        <CardWrapper>
+          {users?.slice(skip, skip + PER_PAGE).map((user) => (
+            <Card key={user.login.uuid}>
+              <img src={user.picture.large} alt="avatar" />
+              <h3>
+                {user.name.first} {""} {user.name.last}
+              </h3>
+              <p>{user.email}</p>
+              <p>@{user.login.username}</p>
+              <div>
+                <p>Phone: {user.cell}</p>
+                <p>Age: {user.dob.age}</p>
+                <p>Country: {user.location.country}</p>
+              </div>
+            </Card>
+          ))}
+        </CardWrapper>
+        <Paginate>
+          <p>
+            Pages: {page} of {pages}
+          </p>
+          <ButtonGroup>
+            <button
+              disabled={page <= 1}
+              onClick={() => setPage((prev) => prev - 1)}
+            >
+              Prev
+            </button>
+            {Array.from({ length: pages }, (value, index) => index + 1).map(
+              (user) => (
+                <button key={user} onClick={() => setPage(user)}>
+                  {user}
+                </button>
+              )
+            )}
+            <button
+              disabled={page >= pages}
+              aria-disabled={page >= pages}
+              onClick={() => setPage((prev) => prev + 1)}
+            >
+              Next
+            </button>
+          </ButtonGroup>
+        </Paginate>
+      </UsersContainer>
+    </UsersWrapper>
+  );
 };
 
-export default index;
+export default Users;
+
+// import React from "react";
+// import { Card, CardWrapper, Paginate, UsersContainer } from "./styles";
+// import picture from "../../assets/lady.png";
+
+// const Users = () => {
+//   return (
+//     <UsersContainer>
+//       <CardWrapper>
+//         <Card>
+//           <img src={picture} alt="" className="avatar" />
+//           <h3>James Bond</h3>
+//           <p>jane@bomgo.com</p>
+//           <p>@emldrf</p>
+//           <div>
+//             <p>Phone: 09039508432</p>
+//             <p>Age: 23</p>
+//             <p>Country: Norway</p>
+//           </div>
+//         </Card>
+//       </CardWrapper>
+//       <Paginate></Paginate>
+//     </UsersContainer>
+//   );
+// };
+
+// export default Users;
--- src/pages/Users/styles.jsx
+++ src/pages/Users/styles.jsx
@@ -1,7 +1,48 @@
 import styled from "styled-components";
 
-export const Users = styled.div``;
-export const Card = styled.div``;
-export const CardWrapper = styled.div``;
-export const Paginate = styled.div``;
-export const UsersWrapper = styled.div``;
+export const UsersWrapper = styled.div`
+  margin: 0 auto;
+`;
+export const UsersContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  width: 95%;
+  margin: 2rem auto;
+`;
+export const CardWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1.5rem;
+  justify-content: center;
+`;
+export const Card = styled.div`
+  width: 25rem;
+  padding: 2rem 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  border: 1px solid #000;
+  border-radius: 1rem;
+  align-items: center;
+  font-family: 1.4rem;
+
+  img {
+    border-radius: 50%;
+  }
+`;
+export const Paginate = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 3rem;
+`;
+export const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 3rem;
+
+  button {
+    width: 4rem;
+  }
+`;
